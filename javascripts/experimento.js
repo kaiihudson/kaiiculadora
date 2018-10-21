7d7cc3c352b7e0b57e3e4338a1eda86d9a5a076d
3d0 <eduardo.cerdanavarrete@cencosud.cl>
2018-10-21 17:17:58 -0300

    Fixes for calculator now the keys works as intended. some logic is faulty, but it can be managed TODO: buttons

--- javascripts/experimento.js
+++ javascripts/experimento.js
@@ -1,27 +1,29 @@
-var buttonArray		=document.querySelectorAll(".key td");
-	operator		=document.querySelectorAll(".op td");
-	input1			=document.querySelector(".firoper");
-	input2			=document.querySelector(".secoper");
-	input3			=document.querySelector(".thioper");
-	result			=document.querySelector(".result td");
+const buttonArray		=document.querySelectorAll(".key td"),
+	operator		=document.querySelectorAll(".op td"),
+	input1			=document.querySelector(".firoper"),
+	input2			=document.querySelector(".secoper"),
+	input3			=document.querySelector(".thioper"),
+	result			=document.querySelector(".result")
 function rootn(){
 	//funcion raiz
-	var x = input1.innerHTML
-		y = input3.innerHTML
+	const 
+		x = input1.innerHTML,
+		y = input3.innerHTML,
 		checkOdd = y % 2;
-	if (checkOdd == 1 || x < 0){
+	if (checkOdd === 1 || x < 0){
 		x = -x;
 	};
-	var resultRoot = x ** 1/y ;
-	var	norm = resultRoot ** y ;
+	const resultRoot = x ** 1/y ;
+	const	norm = resultRoot ** y ;
 	if (Math.abs( x - norm ) && ( x > 0 === norm > 0)){
 		return	checkOdd ? resultRoot: -resultRoot;
 		};
 	};
-function result(){
-	var x = input1.innerHTML;
-		y = input2.innerHTML;
+function resultFunction(){
+	const x = input1.innerHTML,
+		y = input2.innerHTML,
 		z = input3.innerHTML;
+	let equation;
 	//armar la ecuacion
 	switch(y){
 		case "^":
@@ -38,104 +40,105 @@ function result(){
 			break;
 	}
 	if ( x == null || y == null || z ==  null){
-			return
 				alert("you're missing something");
+			return null;
 		} else {
-			return
-				equation
+			return equation
 		}
 	};
 //------------------------------
 //
 //------------------------------
-for(var idArray = 0; idArray < buttonArray.lenght; idArray++){
-	//la cosa para que funcione con el teclado
-	document.onkeypress = function(event){
-		var key = event.charCode;
-		for (var element = 0 ; element <= 10; element++){
-			if (key === (48 + element)){
-				element = input;
-			}
-		}
-		console.log(key);
-		switch(key){
-			case 42:
-        		input2.innerHTML += "*";
-        		break;
-        	//tecla +
-      		case 43:
-        		input2.innerHTML += "+";
-		        break;
-		    //tecla -
-      		case 45:
-        		input2.innerHTML += "-";
-        		break;
-        	//tecla .
-      		case 46:
-        		input2.innerHTML += ".";
-        		break;
-        	//tecla /
-		    case 47:
-        		input2.innerHTML += "/";
-        		break;
-        	//tecla q
-        	case 113:
-        		input2.innerHTML = "sqroot";
-        		break;
-        	//tecla w
-        	case 119:
-        		input2.innerHTML += "^";
-        		break;
-        	//tecla e
-        	case 101:
-        		input2.innerHTML = "^";
-        		input3.innerHTML = "2";
-        		break;
-        	//tecla r
-        	case 114:
-        		input2.innerHTML = "rootn";
-        		break;
-        	//tecla send
-      		case 13:
-      		//tecla =
-		    case 61:
-		    	result();
-      			if (equation) {
-   					try {		
-		    		    result.innerHTML = eval(equation);
-    			   		} 
-	  			  	catch (e) {
-      		 			alert("ERROR");
-    					}
-    				};		    	
-		    	break;
-		    case 67:
-		    // tecla c
-      		case 99:
-		        input1.innerHTML = null;
-		        input2.innerHTML = null;
-		        input3.innerHTML = null;
-		        result.innerHTML = null;
-        		break;
-		    default:
-		    	if (input2.innerHTML != null){
-		    		input3.innerHTML += input
-		    	} else{
-		    		input1.innerHTML += input
-		    	};
-        		break;
-		}
+
+document.onkeypress = function(event){
+	const key = event.charCode;
+	let inputNumbers = input2.innerHTML.trim() === "" ? input1 : input3;
+	if (key > 47 && key < 58){
+		inputNumbers.innerHTML += (key-48)
+		return
 	}
+		
+	switch(key){
+		case 42:
+			input2.innerHTML += "*";
+			break;
+		//tecla +
+		  case 43:
+			input2.innerHTML += "+";
+			break;
+		//tecla -
+		  case 45:
+			input2.innerHTML += "-";
+			break;
+		//tecla .
+		  case 46:
+			input2.innerHTML += ".";
+			break;
+		//tecla /
+		case 47:
+			input2.innerHTML += "/";
+			break;
+		//tecla q
+		case 113:
+			input2.innerHTML = "sqroot";
+			break;
+		//tecla w
+		case 119:
+			input2.innerHTML += "^";
+			break;
+		//tecla e
+		case 101:
+			input2.innerHTML = "^";
+			input3.innerHTML = "2";
+			break;
+		//tecla r
+		case 114:
+			input2.innerHTML = "rootn";
+			break;
+		//tecla send
+		  case 13:
+		  //tecla =
+		case 61:
+			let equation = resultFunction();
+			  if (equation) {
+				   try {		
+					result.innerHTML = "" + eval(equation);
+					   } 
+					catch (e) {
+					   alert("ERROR");
+					}
+				};		    	
+			break;
+		case 67:
+		// tecla c
+		  case 99:
+			input1.innerHTML = null;
+			input2.innerHTML = null;
+			input3.innerHTML = null;
+			result.innerHTML = null;
+			break;
+		default:
+			if (input2.innerHTML != null){
+				input3.innerHTML += input
+			} else{
+				input1.innerHTML += input
+			};
+			break;
+	}
+}
+
+for(let idArray = 0; idArray < buttonArray.length; idArray++){
+	console.log(idArray)
+	//la cosa para que funcione con el teclado
+	
 
 	
 	//la cosa para que funcione con clics
 	buttonArray[idArray].addEventListener('click',function(){
-		var buttonValue	= this.id;
-			inputValue1	= input1.innerHTML;
-			inputValue2 = input2.innerHTML;
+		const buttonValue	= this.id
+		    inputValue1	= input1.innerHTML,
+			inputValue2 = input2.innerHTML,
 			inputValue3 = input3.innerHTML;
-			alert(buttonValue);
-			alert(inputValue);
 			//añade las funciones para clear e igual
 			switch(buttonValue){
 				case "clear":
@@ -145,7 +148,7 @@ for(var idArray = 0; idArray < buttonArray.lenght; idArray++){
 		        result.innerHTML = "";
         		break;
 				case "=":
-					result();
+					resultFunction();
 					break;
 				case "potency":
         			input2.innerHTML += "^";
